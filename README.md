3df4a3aa2c13c7fee487abc776c057b042d109d8
조장혁(laydios) <laydios@stunitas.com>
2021-10-21 15:20:35 +0900

    Swagger setting

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# kotlin + SpringBoot 2.5.5 + Docker Demo
-* kotlin 프로젝트 세팅 및 demo 를 위한 프로젝트
+# kotlin + SpringBoot 2.5.5 + Swagger + Docker Demo
+kotlin 프로젝트 세팅 및 demo 를 위한 프로젝트
+
+## Swagger 
+* http://localhost:8080/swagger-ui/index.html
--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,13 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:3.0.0")
+
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
